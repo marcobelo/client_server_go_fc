3e6fe5df3fc4a819d3a0619d233957956666110e
Marco Belo <marco.barone.belo@gmail.com>
2022-11-21 22:55:26 -0300

    chore: add challenge TODOs

---
+++ client/main.go
@@ -0,0 +1,7 @@
+package client
+
+func main() {
+	//TODO: request to server to retrieve dolar cotation in real
+	//TODO: (timeout max 300ms) wait for server response
+	//TODO: save the current cotation in a file "cotacao.txt" with this format: "Dólar: {valor}/n"
+}
---
+++ server/main.go
@@ -0,0 +1,9 @@
+package server
+
+func main() {
+	//TODO: create the endpoint localhost:8080/cotacao
+	//TODO: consume client request for dolar cotation
+	//TODO: (timeout max 200ms) request dolar cotation to this api: https://economia.awesomeapi.com.br/json/last/USD-BRL
+	//TODO: (timeout max 10ms) save the cotation in a sqlite database
+	//TODO: return to the client only the value (bid)
+}

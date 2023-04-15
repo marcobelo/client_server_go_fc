3793da802bf2c196b7ce5cbb3bc1c448a4068bd3
Marco Belo <marco.barone.belo@gmail.com>
2023-04-15 14:50:28 -0300

    feat: client concluido, chamando /cotacao com timeout de 300ms e salvando resultado em arquivo

--- client/main.go
+++ client/main.go
@@ -1,7 +1,65 @@
-package client
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type CotacaoUSDBRL string
+
+const serverTimeout = 300
 
 func main() {
-	//TODO: request to server to retrieve dolar cotation in real
-	//TODO: (timeout max 300ms) wait for server response
-	//TODO: save the current cotation in a file "cotacao.txt" with this format: "Dólar: {valor}/n"
+	cotacaoUSDBRL, err := GetCotacao()
+	if err != nil {
+		return
+	}
+	newLine := fmt.Sprintf("Dólar: %s\n", *cotacaoUSDBRL)
+	WriteToFile("cotacao.txt", newLine)
+}
+
+func GetCotacao() (*CotacaoUSDBRL, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), serverTimeout*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cotacaoUSDBRL CotacaoUSDBRL
+	err = json.Unmarshal(body, &cotacaoUSDBRL)
+	if err != nil {
+		return nil, err
+	}
+	return &cotacaoUSDBRL, nil
+}
+
+func WriteToFile(filename string, stringToWrite string) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(stringToWrite)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
